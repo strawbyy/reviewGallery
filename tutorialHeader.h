39aa4aa479e8ce46e5522c8e377a5e24aa3d713a
Strawbyy <darkstrawby@gmail.com>
2018-03-23 16:29:02 +0200

    Cleaning up

--- tutorialHeader.h
+++
@@ -1,7 +0,0 @@
-#ifndef TUTORIALHEADER_H
-#define TUTORIALHEADER_H
-// This is here just so that calculatorC.cpp can access it.
-void tutorialChoice();
-void lineBreak();
-
-#endif // TUTORIALHEADER_H
